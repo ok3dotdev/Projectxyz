7e3b8db57c8874340a859cd1bf036e7b02d6ef20
Henry Oke <hoke@LMG-00053.local>
2022-02-28 11:40:15 -0500

    working on hero

---
+++ components/Banner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/Banner.module.css";
+
+const Banner = () => {
+    return (
+        <div>
+            <h1 className={styles.title}>Hi, I'm Henry</h1>
+            <p className={styles.description}>
+                I'm a web-developer based in Toronto, Canada.
+            </p>
+            <div className={styles.button__container}>
+                <button className={styles.button}>Blog</button>
+                <button className={styles.button}>Projects</button>
+                <button className={styles.button}>About Me</button>
+                <button className={styles.button}>Contact</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
