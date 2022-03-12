268e74b47b14fd291608fa9f1a268a0b86e0183e
Henry Oke <hoke@LMG-00053.local>
2022-03-12 16:09:54 -0500

    Fix: Update Postcss to fix vulnerability

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,46 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Script from "next/script";
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
 
-function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+    const router = useRouter()
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }, [router.events])
+  
     return (
-        <>
-            <Script 
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
-            <Script
-            id="GTM-SCRIPTS"
-            strategy="lazyOnload"
-            >
-                
-                {
-                    ` window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')`
-                }
-            </Script>
-            <Component {...pageProps} />
-    </>
+      <>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        <Component {...pageProps} />
+      </>
     )
-}
+  }
 
-export default MyApp;
+export default App;
